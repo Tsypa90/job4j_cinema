c99ed26c0ff532f453fbd76aba701c86e61bab74
Tsypa90 <URSA2004@mail.ru>
2022-08-01 12:03:56 +0300

    2. Сервис - Кинотеатр [#504869 #326647]

--- db/scripts/update_001.sql
+++ db/scripts/update_001.sql
@@ -1,16 +1,16 @@
-CREATE TABLE users (
+CREATE TABLE if not exists users (
   id SERIAL PRIMARY KEY,
   username VARCHAR NOT NULL,
   email VARCHAR NOT NULL UNIQUE,
   phone VARCHAR NOT NULL UNIQUE
 );
 
-CREATE TABLE films (
+CREATE TABLE if not exists films (
   id SERIAL PRIMARY KEY,
   name text
 );
 
-CREATE TABLE ticket (
+CREATE TABLE if not exists ticket (
     id SERIAL PRIMARY KEY,
     session_id INT NOT NULL REFERENCES films(id),
     pos_row INT NOT NULL unique,
@@ -18,7 +18,7 @@ CREATE TABLE ticket (
     user_id INT NOT NULL REFERENCES users(id)
 );
 
-create table cinema (
+create table if not exists cinema (
 	session_id int not null references sessions(id),
 	row int not null,
 	seat int not null
